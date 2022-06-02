6913807a3aaa5d099738a299958ac42261f44854
WizzyVeigar <48510818+WizzyVeigar@users.noreply.github.com>
2022-06-02 12:29:40 +0200

    Testing Github desktop

--- app/src/main/java/com/example/dobotappcontroller/DobotConnector.java
+++ app/src/main/java/com/example/dobotappcontroller/DobotConnector.java
@@ -1,7 +1,7 @@
 package com.example.dobotappcontroller;
 
 import android.widget.TextView;
-
+//This is a test comment for testing
 public class DobotConnector {
     public boolean ConnectToDobot(){
         return false;
